80630153ab4fd71e708961f9985b377ea15156c4
WindDogg <1150547471@qq.com>
2019-06-19 17:36:58 +0800

    update jdbc

--- src/main/java/com/xue/onepiece/OnepieceApplication.java
+++ src/main/java/com/xue/onepiece/OnepieceApplication.java
@@ -8,7 +8,7 @@ public class OnepieceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OnepieceApplication.class, args);
-        String a="hello world!";
+        System.out.println("hello,wlord!");
     }
 
 }
