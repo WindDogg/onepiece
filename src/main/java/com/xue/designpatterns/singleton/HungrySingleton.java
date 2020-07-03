fd082dd480603ea1c3a122da3994761d9ae9cf6b
WindDogg <1150547471@qq.com>
2020-07-03 15:06:06 +0800

    单例模式-懒汉、饿汉

---
+++ src/main/java/com/xue/designpatterns/singleton/HungrySingleton.java
@@ -0,0 +1,10 @@
+package com.xue.designpatterns.singleton;
+
+public class HungrySingleton {
+    private static final HungrySingleton instance = new HungrySingleton();
+    private HungrySingleton(){}
+    public static HungrySingleton getInstance()
+    {
+        return instance;
+    }
+}
